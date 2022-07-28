c2f35c651f226c137c99d33de4042d11669cfad6
tjd39 <tom.dennis.1995@gmail.com>
2022-07-28 15:24:47 +0100

    merge

--- screen.lua
+++ screen.lua
@@ -43,11 +43,8 @@ function update_game()
     move_bees()
     update_user_sprite()
     update_lazers()
-<<<<<<< HEAD
     update_rockets()
-=======
     check_dead_bear()
->>>>>>> ba0090576b41f19dfa8e5846470c3b5b758fe5ce
 end
 
 function draw_game()
