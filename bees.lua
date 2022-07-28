2877d497488b3b70067eda84e1daefcc7bd6cee0
Joshua Renton <joshuarenton@Joshuas-MacBook-Pro.local>
2022-07-28 13:07:33 +0100

    Tidy up bees

--- bees.lua
+++ bees.lua
@@ -3,26 +3,27 @@ bees = {}
 function spawn_bee()
     if(time() % 2 == 0) then 
         half_screen = screen_width/2
-        bee = {x=screen_width, 
-            y=rnd(screen_height - tile_width)}
+        bee = {
+            sprite=1,
+            x=screen_width, 
+            y=rnd(screen_height - tile_width)
+        }
         add(bees, bee)
     end
 end
 
 function move_bees()
-    if(true) then 
-        foreach(bees, move_bee)
-    end
+    foreach(bees, move_bee)
 end
 
 function move_bee(bee)
     if (bee.x < -tile_width) then 
         del(bees, bee)
     else
-        bee.x = bee.x - 1
+        bee.x -= 1.5
     end
 end
 
 function draw_bee(bee)
-    spr(1, bee.x, bee.y)
+    spr(bee.sprite, bee.x, bee.y)
 end
