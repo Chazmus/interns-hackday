caee6c56edfd4092f3083a97ea2ea0955e1cd9d2
Joshua Renton <joshuarenton@Joshuas-MacBook-Pro.local>
2022-07-28 14:17:01 +0100

    added bees spawn rate

--- bees.lua
+++ bees.lua
@@ -1,11 +1,12 @@
 bees = {}
+spawn_rate = 2
 
 function spawn_bee()
-    if(time() % 2 == 0) then 
+    if(time() % (1 / spawn_rate) == 0) then 
         half_screen = screen_width/2
         bee = {
             sprite=1,
-            x=screen_width, 
+            x=screen_width,
             y=rnd(screen_height - tile_width - 2) + 1,
             tick = 0
         }
@@ -17,19 +18,23 @@ function move_bees()
     foreach(bees, move_bee)
 end
 
-function move_bee(bee)
+function move_bee(bee)    
     if (bee.x < -tile_width) then 
         del(bees, bee)
     else
-        bee.tick += 1
-        bee.x -= 1.5
+        if bee.tick == 100 then
+            bee.tick = 1
+        else
+            bee.tick += 1
+            bee.x -= 1.5
+        end
     end
 end
 
 function draw_bee(bee)
-    if bee.tick % 2 == 0 then
+    if bee.tick % 4 == 0 then
         bee.y -= 1
-    else
+    elseif bee.tick % 2 == 0 then
         bee.y += 1
     end
 
