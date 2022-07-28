542f951b7850b6120997d08cdaf8f5aa0a49c405
Joshua Renton <joshuarenton@Joshuas-MacBook-Pro.local>
2022-07-28 13:33:07 +0100

    added vibrating bees

--- bees.lua
+++ bees.lua
@@ -6,7 +6,7 @@ function spawn_bee()
         bee = {
             sprite=1,
             x=screen_width, 
-            y=rnd(screen_height - tile_width),
+            y=rnd(screen_height - tile_width - 2) + 1,
             tick = 0
         }
         add(bees, bee)
@@ -27,6 +27,12 @@ function move_bee(bee)
 end
 
 function draw_bee(bee)
+    if bee.tick % 2 == 0 then
+        bee.y -= 1
+    else
+        bee.y += 1
+    end
+
     if bee.tick % 5 == 0 then
         if bee.sprite == 1 then 
             bee.sprite = 2
